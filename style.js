b6481f72662745d6fd5afa11bd81a53349d48369
nhlong23051 <nhlong23051@gmail.com>
2023-04-11 18:44:51 +0700

    capstone

---
+++ style.js
@@ -0,0 +1,6 @@
+function themeDark (){
+    console.log(123);
+    document.getElementById('themeLife').className = "themeDark";
+    document.getElementById('iconDark').style.display = 'none'
+    document.getElementById('iconLight').style.display = 'inline'
+}
