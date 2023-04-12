020ef384d85b3137905bde55cfe7d519d206abd7
nhlong23051 <nhlong23051@gmail.com>
2023-04-12 11:32:23 +0700

    Light/Dark Mode

--- style.js
+++ style.js
@@ -1,6 +1,19 @@
-function themeDark (){
-    console.log(123);
-    document.getElementById('themeLife').className = "themeDark";
-    document.getElementById('iconDark').style.display = 'none'
-    document.getElementById('iconLight').style.display = 'inline'
+var themeBTN = document.getElementById('theme');
+themeBTN.onclick = function (){
+    document.getElementById('themeLife').className = 'dark';
+    document.getElementById('services__block').className = 'dark';
+    document.getElementById('index__block').className = 'dark';
+    document.getElementById('navSwith').className = 'dark';
+    document.getElementById('theme1').style.display = 'inline-block'
+    document.getElementById('theme').style.display = 'none'
+}
+
+var theme1 = document.getElementById('theme1');
+theme1.onclick = function () {
+    document.getElementById('themeLife').className = 'active';
+    document.getElementById('services__block').className = 'active';
+    document.getElementById('index__block').className = 'active';
+    document.getElementById('navSwith').className = 'active';
+    document.getElementById('theme').style.display = 'inline-block'
+    document.getElementById('theme1').style.display = 'none'
 }
